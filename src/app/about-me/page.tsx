ac41cb5abad12afb814b489efc39882fbf0a27a3
Rocio Alcala <rro.alcala@gmail.com>
2024-07-04 19:54:27 -0300

    about me page

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,16 +1,15 @@
+import styles from "./AboutMe.module.css";
+
 export default function AboutMe() {
   return (
-    <div className="fadeIn">
-      <h1></h1>
-      <p>
-        Soy una persona entusiasta y curiosa, siempre en busca de nuevas
-        experiencias y conocimientos. Me encanta conectar con personas de
-        diferentes culturas y aprender de sus historias y tradiciones. Soy
-        creativa, con una mente abierta y un corazón compasivo, siempre
-        dispuesta a ayudar a los demás y a hacer del mundo un lugar mejor. Me
-        apasiona el arte en todas sus formas y disfruto tanto de la tranquilidad
-        de un buen libro como de la emoción de una aventura al aire libre.
-      </p>
-    </div>
+    <p className={`${styles.globo} fadeIn`}>
+      Soy una persona entusiasta y curiosa, siempre en busca de nuevas
+      experiencias y conocimientos. Me encanta conectar con personas de
+      diferentes culturas y aprender de sus historias y tradiciones. Soy
+      creativa, con una mente abierta y un corazón compasivo, siempre dispuesta
+      a ayudar a los demás y a hacer del mundo un lugar mejor. Me apasiona el
+      arte en todas sus formas y disfruto tanto de la tranquilidad de un buen
+      libro como de la emoción de una aventura al aire libre.
+    </p>
   );
 }
